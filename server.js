af8c969c3c55b38b91ad471b152543bb188cbeea
LeonNguyen <Tuan.Nguyen@fortressgb.com>
2022-10-24 17:27:35 +0700

    deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/hacker-angular'));
+app.use(express.static(__dirname + '/dist/hacker-angular/'));
 
 app.get('/*', function(req,res) {
 
